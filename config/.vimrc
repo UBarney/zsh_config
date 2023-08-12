95fce38ebe4de7c91078197d5b17d534bf72443f
UBarney <UBarney@users.noreply.github.com>
2023-08-12 16:33:06 +0800

    refactor

---
+++ config/.vimrc
@@ -0,0 +1,9 @@
+set mouse-=a
+set number
+set relativenumber
+
+set ignorecase
+set smartcase
+
+set hlsearch
+hi Search cterm=none ctermbg=green ctermfg=white guibg=lightgreen guifg=black
---
+++ setup.bash
@@ -0,0 +1,10 @@
+sudo apt update 
+sudo apt install -y curl zsh git
+echo n|sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -L git.io/antigen > ~/script/antigen.zsh
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+echo y | ~/.fzf/install
+
+cd ~
+git clone https://github.com/UBarney/zsh_config
+cp -r ~/config/config/* ~
